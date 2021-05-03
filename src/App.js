a0a7cfed68f581fe6e24f4f8d01ff31eca01ea77
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-05-03 14:58:04 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Its so fun
+          Learn React Its 
         </a>
       </header>
     </div>
